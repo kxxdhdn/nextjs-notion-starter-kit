2b798e720267407aa81cfb79f01e0b88172da7d4
OOPA <zayintopology@gmail.com>
2022-09-21 17:56:22 +0200

    v1.0.2 favicon setup via Head component

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react'
-import Document, { Html, Main, NextScript } from 'next/document'
+import Document, { Html, Head, Main, NextScript } from 'next/document'
 import { IconContext } from '@react-icons/all-files'
 
 export default class MyDocument extends Document {
@@ -29,6 +29,13 @@ export default class MyDocument extends Document {
 
             <link rel='manifest' href='/manifest.json' />
           </Head>*/}
+          <link rel='apple-touch-icon' sizes='180x180' href='/apple-touch-icon.png' />
+          <link rel='icon' type='image/png' sizes='32x32' href='/favicon-32x32.png' />
+          <link rel='icon' type='image/png' sizes='16x16' href='/favicon-16x16.png' />
+          <link rel='manifest' href='/site.webmanifest' />
+          <link rel='mask-icon' href='/safari-pinned-tab.svg' color='#5bbad5' />
+          <meta name='msapplication-TileColor' content='#da532c' />
+          <meta name='theme-color' content='#ffffff' />
 
           <body>
             <script
